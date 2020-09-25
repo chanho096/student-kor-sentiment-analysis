a60f8d38afc86470d4523948fdf14ce69c0f93cc
chanho <chanho960122@gmail.com>
2020-09-26 04:10:02 +0900

    Test

--- test.py
+++ test.py
@@ -33,6 +33,8 @@ DP_MAX_WORD_LENGTH = loader.dp_max_word_length
 DP_LABEL_SYNTAX_LENGTH = len(DP_LABEL_SYNTAX_DICT)
 DP_LABEL_FUNCTION_LENGTH = len(DP_LABEL_FUNCTION_DICT)
 
+dp_model_path = "DP_model.pt"
+
 
 class MultipleSoftmax(torch.nn.Module):
     def __init__(self,
@@ -57,7 +59,8 @@ class MultipleSoftmax(torch.nn.Module):
             x = input_tensor[:, :, idx]
             out = self.dropouts[idx](x) if self.dr_rate else x
             out = classifier(out)
-            out = torch.nn.functional.softmax(out)
+            out = torch.nn.functional.softmax(out, dim=1)
+
             result.append(out)
 
         return result
@@ -96,7 +99,47 @@ class SimpleDP(torch.nn.Module):
         result_0 = self.multiple_softmax_0(word_vectors)
         result_1 = self.multiple_softmax_1(word_vectors)
 
-        return word_vectors, result_0, result_1
+        return pooler, word_vectors, result_0, result_1
+
+
+class DPSA(torch.nn.Module):
+    def __init__(self,
+                 simple_dp,
+                 num_hiddens=768,
+                 dr_rate_0=None,
+                 dr_rate_1=None
+                 ):
+        super(DPSA, self).__init__()
+        self.simple_dp = simple_dp
+        self.dr_rate_0 = dr_rate_0
+        self.dr_rate_1 = dr_rate_1
+
+        self.lstm = torch.nn.LSTM(input_size=num_hiddens + DP_LABEL_SYNTAX_LENGTH + DP_LABEL_FUNCTION_LENGTH,
+                                  hidden_size=num_hiddens, dropout=dr_rate_0 if dr_rate_0 else 0)
+        self.classifier = torch.nn.Linear(num_hiddens*2, 2)
+
+        if dr_rate_1:
+            self.dropout_1 = torch.nn.Dropout(p=dr_rate_1)
+
+    def forward(self, x, segment_ids, attention_mask, sa=True, absa=False):
+        # bert forward
+        pooler, word_vectors, result_0, result_1 = self.simple_dp(x, segment_ids, attention_mask)
+
+        result_stack_0 = torch.stack(result_0, dim=2)
+        result_stack_1 = torch.stack(result_1, dim=2)
+        lstm_input = torch.cat((word_vectors, result_stack_0, result_stack_1), dim=1)
+        lstm_input = torch.transpose(lstm_input, 1, 2)
+        lstm_input = torch.transpose(lstm_input, 0, 1)  # (seq_len, batch, input_size)
+
+        lstm_output, _ = self.lstm(lstm_input)
+        last_lstm_output = lstm_output[-1, :, :]
+
+        classifier_input = torch.cat((pooler, last_lstm_output), dim=1)
+        out = self.dropout_1(classifier_input) if self.dr_rate_1 else classifier_input
+        out = self.classifier(out)
+        out = torch.nn.functional.softmax(out, dim=1)
+
+        return out
 
 
 def dp_label_encoder(label_list):
@@ -198,7 +241,7 @@ def ex_dp_training(ctx="cuda:0", opt=masa.model.DEFAULT_OPTION):
             x = word_embedding(token_ids)
 
             # forward propagation
-            word_vector, result_0, result_1 = model(x, segment_ids, attention_mask)
+            _, word_vector, result_0, result_1 = model(x, segment_ids, attention_mask)
 
             # backward propagation
             loss = lf(result_0[0], label_0[:, 0])
@@ -244,7 +287,7 @@ def ex_dp_training(ctx="cuda:0", opt=masa.model.DEFAULT_OPTION):
                 x = word_embedding(token_ids)
 
                 # forward propagation
-                word_vector, result_0, result_1 = model(x, segment_ids, attention_mask)
+                _, word_vector, result_0, result_1 = model(x, segment_ids, attention_mask)
 
                 # backward propagation
                 loss = lf(result_0[0], label_0[:, 0])
@@ -267,19 +310,136 @@ def ex_dp_training(ctx="cuda:0", opt=masa.model.DEFAULT_OPTION):
         torch.save(model.state_dict(), f"pre_trained_model_{e + 1}.pt")
 
 
+def ex_pre_training(opt=masa.model.DEFAULT_OPTION, ctx="cuda:0"):
+    # ------------------------------------------
+    # load bert
+    bert_model, vocab = kobert.pytorch_kobert.get_pytorch_kobert_model()
+    bert_tokenizer = masa.model.get_bert_tokenizer(vocab)
+    transform = nlp.data.BERTSentenceTransform(
+        bert_tokenizer, max_seq_length=opt["max_len"], pad=True, pair=False)
+
+    # ---------------------- Data Loader -----------------------
+    # ----------------------------------------------------------
+    # Naver sentiment movie corpus v1.0
+    data_path = loader.download_movie_corpus_data()
+
+    batch_loaders = []
+    for path in data_path:
+        dataset = nlp.data.TSVDataset(path, field_indices=[1, 2], num_discard_samples=1)
+
+        corpus_list = []
+        label_list = []
+        sentence_info = []
+        for (corpus, label) in list(dataset):
+            corpus_list.append(corpus)
+
+            # set label array
+            label_array = np.array(label, dtype=np.int32)
+
+            # set label list
+            label_list.append(label_array)
+
+            # tokenizing
+            sentence = transform([corpus])
+            sentence_info.append(sentence)
+
+        for idx, tuple_item in enumerate(sentence_info):
+            sentence_info[idx] = tuple_item + (label_list[idx],)
+
+        batch_loader = torch.utils.data.DataLoader(sentence_info, batch_size=opt["batch_size"], num_workers=0)
+        batch_loaders.append(batch_loader)
+
+    train_batch_loader, test_batch_loader = batch_loaders
+
+    # ----------------- ABSA CLASSIFIER MODEL ------------------
+    # ----------------------------------------------------------
+    device = torch.device(ctx)
+
+    simple_dp_model = SimpleDP(bert_model).to(device)
+    # simple_dp_model.load_state_dict(torch.load(dp_model_path, map_location=self.device))
+    model = DPSA(simple_dp_model).to(device)
+
+    no_decay = ['bias', 'LayerNorm.weight']
+    optimizer_grouped_parameters = [
+        {'params': [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)],
+         'weight_decay': 0.01},
+        {'params': [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
+    ]
+    optimizer = transformers.AdamW(optimizer_grouped_parameters, lr=opt["learning_rate"])
+    lf = torch.nn.CrossEntropyLoss()
+
+    t_total = len(train_batch_loader) * opt["num_epochs"]
+    warmup_steps = int(t_total * opt["warmup_ratio"])
+    scheduler = transformers.optimization.get_linear_schedule_with_warmup(optimizer, warmup_steps, t_total)
+
+    # -------------- ABSA CLASSIFIER MODEL TRAIN ---------------
+    # ----------------------------------------------------------
+    for e in range(opt["num_epochs"]):
+        train_accuracy = 0.0
+        test_accuracy = 0.0
+
+        # Train Batch
+        model.train()
+        for batch_id, (token_ids, valid_length, segment_ids, label) in enumerate(train_batch_loader):
+            optimizer.zero_grad()
+
+            # set train batch
+            token_ids = token_ids.long().to(device)
+            segment_ids = segment_ids.long().to(device)
+            valid_length = valid_length
+            label = label.long().to(device)
+
+            # get word embedding
+            attention_mask = masa.model.gen_attention_mask(token_ids, valid_length)
+            word_embedding = simple_dp_model.bert.get_input_embeddings()
+            x = word_embedding(token_ids)
+
+            # forward propagation
+            out = model(x, segment_ids, attention_mask, sa=True, absa=True)
+
+            # backward propagation
+            loss = lf(out, label)
+            loss.backward()
+
+            # optimization
+            torch.nn.utils.clip_grad_norm_(model.parameters(), opt["max_grad_norm"])
+            optimizer.step()
+            scheduler.step()  # Update learning rate schedule
+            train_accuracy += masa.model.calculate_accuracy(out, label)
+
+            if batch_id % opt["log_interval"] == 0:
+                print("epoch {} batch id {} loss {} train accuracy {}".format(e + 1, batch_id + 1,
+                                                                              loss.data.cpu().numpy(),
+                                                                              train_accuracy / (batch_id + 1)))
+        print("epoch {} train accuracy {}".format(e + 1, train_accuracy / (batch_id + 1)))
+
+        # Test Batch
+        model.eval()
+        with torch.no_grad():
+            for batch_id, (token_ids, valid_length, segment_ids, label) in enumerate(test_batch_loader):
+                # set test batch
+                token_ids = token_ids.long().to(device)
+                segment_ids = segment_ids.long().to(device)
+                valid_length = valid_length
+                label = label.long().to(device)
+
+                # get word embedding
+                attention_mask = masa.model.gen_attention_mask(token_ids, valid_length)
+                word_embedding = model.bert.get_input_embeddings()
+                x = word_embedding(token_ids)
+
+                # forward propagation
+                out = model(x, segment_ids, attention_mask, sa=True, absa=True)
+
+                # test accuracy
+                test_accuracy += masa.model.calculate_accuracy(out, label)
+            print("epoch {} test accuracy {}".format(e + 1, test_accuracy / (batch_id + 1)))
+
+        torch.save(model.state_dict(), f"pre_trained_model_{e+1}.pt")
+
+
 if __name__ == '__main__':
-    """
-    a, b, c = loader.load_dependency_parsing_data()
-
-    label_type = []
-    for ll in b:
-        for l in ll:
-            if l not in label_type:
-                label_type.append(l)
-
-    print(label_type)
-    """
-    ex_dp_training()
+    ex_pre_training()
 
 
 
